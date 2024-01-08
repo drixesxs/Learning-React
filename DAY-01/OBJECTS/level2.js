87a7fbdff5dfeb40c654cb499e982aeba0ddb04a
drixesxs <oluwatimiseyi@gmail.com>
2024-01-08 10:02:42 +0100

    learning objects level2.js finished

--- DAY-01/OBJECTS/level2.js
+++ DAY-01/OBJECTS/level2.js
@@ -53,7 +53,7 @@ const users = {
 
 
 const usersWith50PointsOrMore = Object.values(users).filter(user => user.points >= 50).length;
-console.log(usersWith50PointsOrMore)
+
 
 let mostSkilledPerson = '';
 let maxSkills = 0;
@@ -65,4 +65,91 @@ for (const user in users) {
   }
 }
 
-console.log(mostSkilledPerson)
+
+merndev = []
+for (const user in users) {
+        if (users[user].skills.includes("Express", "MongoDB", "React", "Node")) {
+            merndev.push(user)
+        }
+    }
+
+    
+
+
+const newusers = {
+    Alex: {
+        email: 'alex@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        age: 20,
+        isLoggedIn: false,
+        points: 30
+    },
+    Asab: {
+        email: 'asab@asab.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+    },
+    Brook: {
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 30,
+        isLoggedIn: true,
+        points: 50
+    },
+    Daniel: {
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    John: {
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+    },
+    Thomas: {
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    Paul: {
+        email: 'paul@paul.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    Timi: {
+        email: 'timi@timi.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    }
+}
+
+
+const countries = {
+    nigeria: {
+        captial: 'abuja',
+        population: 1000,
+        languages: ['english', 'yoruba']
+    },
+
+    uk: {
+        caapital: 'london',
+        population: 5000,
+        languages: ['english', 'mandem yoruba']
+    }
+}
+
+
+console.log(countries)
+
