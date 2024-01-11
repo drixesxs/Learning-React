afe478552a5f317576d5de430f4ce5fd36d0ac1a
drixesxs <oluwatimiseyi@gmail.com>
2024-01-11 17:07:05 +0100

    functions level1.js

---
+++ DAY-01/FUNCTIONS/level1.js
@@ -0,0 +1,16 @@
+function fullName(firstName, lastName){
+    return `${firstName} ${lastName}`
+}
+
+console.log(fullName('Timileyin', 'Ogunjimi'))
+
+function sum(num1, num2){
+    return num1 + num2
+}
+
+console.log(sum(2, 2))
+
+function areaOfCircle(r){
+    return 3.14 * (r^2) 
+}
+
